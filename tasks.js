63d0df2c7b878a403f858da85d6dbe0d3b345c8e
Siam Sheikh <siammsheikh09@gmail.com>
2025-03-02 23:56:58 +0600

    final push

--- tasks.js
+++ tasks.js
@@ -1,8 +1,203 @@
-const completedBtns = document.getElementsByClass('completed-btn');
-console.groupCollapsed(completedBtns);
+document.getElementById('task-btn-1').addEventListener('click', function(){
+    alert('Board Updated Succesfully');
 
-// completedBtns.addEventListener('click', function(){
-//     for(const completedBtn of completedBtns){
-//         console.log("click")
-//     }
-// })
+    this.disabled = true;
+
+    assignedTasks = parseInt(document.getElementById('assigned-tasks').innerText);
+    remainingTasks = assignedTasks - 1;
+    document.getElementById('assigned-tasks').innerText = remainingTasks;
+
+    if(assignedTasks <= 0){
+        alert('All tasks have been completed.');
+    }
+
+    completedTasks = parseInt(document.getElementById('completed-tasks').innerText);
+    totalTasks = completedTasks + 1;
+    document.getElementById('completed-tasks').innerText = totalTasks;
+
+    activityLog = document.getElementById('activity-log');
+    const newParagraph = document.createElement('p');
+    const currentTime = new Date().toLocaleTimeString();
+    newParagraph.textContent = `You have completed Fix Mobile Button Issue task at ${currentTime}`;
+    newParagraph.style.backgroundColor = 'aliceBlue';
+    newParagraph.style.borderRadius = '5px';
+    newParagraph.style.padding = '5px';
+    newParagraph.style.margin = '5px';
+    activityLog.appendChild(newParagraph);
+});
+
+document.getElementById('task-btn-2').addEventListener('click', function(){
+    alert('Board Updated Succesfully');
+
+    this.disabled = true;
+
+    assignedTasks = parseInt(document.getElementById('assigned-tasks').innerText);
+    remainingTasks = assignedTasks - 1;
+    document.getElementById('assigned-tasks').innerText = remainingTasks;
+
+    if(assignedTasks <= 0){
+        alert('All tasks have been completed.');
+    }
+
+    completedTasks = parseInt(document.getElementById('completed-tasks').innerText);
+    totalTasks = completedTasks + 1;
+    document.getElementById('completed-tasks').innerText = totalTasks;
+
+    activityLog = document.getElementById('activity-log');
+    const newParagraph = document.createElement('p');
+    const currentTime = new Date().toLocaleTimeString();
+    newParagraph.textContent = `You have completed Add Dark Mode task at ${currentTime}`;
+    newParagraph.style.backgroundColor = 'aliceBlue';
+    newParagraph.style.borderRadius = '5px';
+    newParagraph.style.padding = '5px';
+    newParagraph.style.margin = '5px';
+    activityLog.appendChild(newParagraph);
+});
+
+document.getElementById('task-btn-3').addEventListener('click', function(){
+    alert('Board Updated Succesfully');
+
+    this.disabled = true;
+
+    assignedTasks = parseInt(document.getElementById('assigned-tasks').innerText);
+    remainingTasks = assignedTasks - 1;
+    document.getElementById('assigned-tasks').innerText = remainingTasks;
+
+    if(assignedTasks <= 0){
+        alert('All tasks have been completed.');
+    }
+
+    completedTasks = parseInt(document.getElementById('completed-tasks').innerText);
+    totalTasks = completedTasks + 1;
+    document.getElementById('completed-tasks').innerText = totalTasks;
+
+    activityLog = document.getElementById('activity-log');
+    const newParagraph = document.createElement('p');
+    const currentTime = new Date().toLocaleTimeString();
+    newParagraph.textContent = `You have completed Optimize  Home page task at ${currentTime}`;
+    newParagraph.style.backgroundColor = 'aliceBlue';
+    newParagraph.style.borderRadius = '5px';
+    newParagraph.style.padding = '5px';
+    newParagraph.style.margin = '5px';
+    activityLog.appendChild(newParagraph);
+});
+
+document.getElementById('task-btn-4').addEventListener('click', function(){
+    alert('Board Updated Succesfully');
+
+    this.disabled = true;
+
+    assignedTasks = parseInt(document.getElementById('assigned-tasks').innerText);
+    remainingTasks = assignedTasks - 1;
+    document.getElementById('assigned-tasks').innerText = remainingTasks;
+
+    if(assignedTasks <= 0){
+        alert('All tasks have been completed.');
+    }
+
+    completedTasks = parseInt(document.getElementById('completed-tasks').innerText);
+    totalTasks = completedTasks + 1;
+    document.getElementById('completed-tasks').innerText = totalTasks;
+
+    activityLog = document.getElementById('activity-log');
+    const newParagraph = document.createElement('p');
+    const currentTime = new Date().toLocaleTimeString();
+    newParagraph.textContent = `You have completed Add new emoji 🤲 task at ${currentTime}`;
+    newParagraph.style.backgroundColor = 'aliceBlue';
+    newParagraph.style.borderRadius = '5px';
+    newParagraph.style.padding = '5px';
+    newParagraph.style.margin = '5px';
+    activityLog.appendChild(newParagraph);
+});
+
+document.getElementById('task-btn-5').addEventListener('click', function(){
+    alert('Board Updated Succesfully');
+
+    this.disabled = true;
+
+    assignedTasks = parseInt(document.getElementById('assigned-tasks').innerText);
+    remainingTasks = assignedTasks - 1;
+    document.getElementById('assigned-tasks').innerText = remainingTasks;
+
+    if(assignedTasks <= 0){
+        alert('All tasks have been completed.');
+    }
+
+    completedTasks = parseInt(document.getElementById('completed-tasks').innerText);
+    totalTasks = completedTasks + 1;
+    document.getElementById('completed-tasks').innerText = totalTasks;
+
+    activityLog = document.getElementById('activity-log');
+    const newParagraph = document.createElement('p');
+    const currentTime = new Date().toLocaleTimeString();
+    newParagraph.textContent = `You have completed Integrate OpenAI API task at ${currentTime}`;
+    newParagraph.style.backgroundColor = 'aliceBlue';
+    newParagraph.style.borderRadius = '5px';
+    newParagraph.style.padding = '5px';
+    newParagraph.style.margin = '5px';
+    activityLog.appendChild(newParagraph);
+});
+
+document.getElementById('task-btn-6').addEventListener('click', function(){
+    alert('Board Updated Succesfully');
+
+    this.disabled = true;
+
+    assignedTasks = parseInt(document.getElementById('assigned-tasks').innerText);
+    remainingTasks = assignedTasks - 1;
+    document.getElementById('assigned-tasks').innerText = remainingTasks;
+
+    if(assignedTasks <= 0){
+        alert('All tasks have been completed.');
+    }
+
+    completedTasks = parseInt(document.getElementById('completed-tasks').innerText);
+    totalTasks = completedTasks + 1;
+    document.getElementById('completed-tasks').innerText = totalTasks;
+
+    activityLog = document.getElementById('activity-log');
+    const newParagraph = document.createElement('p');
+    const currentTime = new Date().toLocaleTimeString();
+    newParagraph.textContent = `You have completed Improve Job searching task at ${currentTime}`;
+    newParagraph.style.backgroundColor = 'aliceBlue';
+    newParagraph.style.borderRadius = '5px';
+    newParagraph.style.padding = '5px';
+    newParagraph.style.margin = '5px';
+    activityLog.appendChild(newParagraph);
+});
+
+document.getElementById('task-btn-1').addEventListener('click', function(){
+    alert('Board Updated Succesfully');
+
+    this.disabled = true;
+
+    assignedTasks = parseInt(document.getElementById('assigned-tasks').innerText);
+    remainingTasks = assignedTasks - 1;
+    document.getElementById('assigned-tasks').innerText = remainingTasks;
+
+    if(assignedTasks <= 0){
+        alert('All tasks have been completed.');
+    }
+
+    completedTasks = parseInt(document.getElementById('completed-tasks').innerText);
+    totalTasks = completedTasks + 1;
+    document.getElementById('completed-tasks').innerText = totalTasks;
+
+    activityLog = document.getElementById('activity-log');
+    const newParagraph = document.createElement('p');
+    const currentTime = new Date().toLocaleTimeString();
+    newParagraph.textContent = `You have completed Fix Mobile Button Issue task at ${currentTime}`;
+    newParagraph.style.backgroundColor = 'aliceBlue';
+    newParagraph.style.borderRadius = '5px';
+    newParagraph.style.padding = '5px';
+    newParagraph.style.margin = '5px';
+    activityLog.appendChild(newParagraph);
+});
+
+
+
+document.getElementById('activity-btn').addEventListener('click', function(){
+    activityLog = document.getElementById('activity-log');
+    activityLog.removeChild(this.childNodes);
+
+})
